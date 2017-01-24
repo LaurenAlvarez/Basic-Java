d7a1b6970a17f75d88793c8cf22e47ecc0873e4d
LaurenAlvarez <lalvar19@lion.lmu.edu>
2017-01-24 10:32:07 -0800

    Add files via upload

--- Basic-Java/ReplaceOddWords.java
+++ Basic-Java/ReplaceOddWords.java
@@ -4,9 +4,12 @@ public class ReplaceOddWords {
     public static void main (String[] args) {
         int numOfArgs = args.length;
             for (int i = 0; i < numOfArgs; i++) {
-                if (i % 2 == 0) {
-                    System.out.print (args[i] + " and. ");
+                if (i % 2 == 0)
+                    System.out.print (args[i]);
+
+                    else
+                        System.out.print (" and ");
+
                 }
             }
         }
-    }
